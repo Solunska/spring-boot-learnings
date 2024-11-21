61fd6883876ff9da5f362557b24468ff1129529a
solunska <a_solunska@outlook.com>
2024-11-21 14:53:05 +0100

    added devtools and actuator dependencies

--- 01. Spring Boot Overview/src/main/java/com/springboot/demo/myapp/rest/FunRestController.java
+++ 01. Spring Boot Overview/src/main/java/com/springboot/demo/myapp/rest/FunRestController.java
@@ -11,4 +11,10 @@ public class FunRestController {
     public String sayHello() {
         return "Hello World";
     }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "This is a workout";
+    }
 }

9a5688bc1b5c90c02122e5f7ac95cc734b5ead18
solunska <a_solunska@outlook.com>
2024-11-22 16:13:45 +0100

    injecting custom application properties

--- 01. Spring Boot Overview/src/main/java/com/springboot/demo/myapp/rest/FunRestController.java
+++ 01. Spring Boot Overview/src/main/java/com/springboot/demo/myapp/rest/FunRestController.java
@@ -1,10 +1,17 @@
 package com.springboot.demo.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${student.name}")
+    private String name;
+
+    @Value("${student.surname}")
+    private String surname;
 
     // expose "/" endpoint that will return "Hello World"
     @GetMapping("/")
@@ -16,5 +23,10 @@ public class FunRestController {
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "This is a workout";
+    }
+
+    @GetMapping("/studentInfo")
+    public String getTeamInfo() {
+        return "Name: " + name + " " + surname;
     }
 }

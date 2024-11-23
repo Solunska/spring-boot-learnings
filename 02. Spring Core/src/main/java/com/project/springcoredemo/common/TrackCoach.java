702eff8dd0e1a94fe07c95d41852ec54b9c4d38d
solunska <a_solunska@outlook.com>
2024-11-23 14:11:12 +0100

    learning about lazy initialization

--- 02. Spring Core/src/main/java/com/project/springcoredemo/common/TrackCoach.java
+++ 02. Spring Core/src/main/java/com/project/springcoredemo/common/TrackCoach.java
@@ -1,11 +1,16 @@
 package com.project.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@Lazy
 public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("In constructor: " + this.getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
--- 02. Spring Core/src/main/java/com/project/springcoredemo/rest/DemoController.java
+++ 02. Spring Core/src/main/java/com/project/springcoredemo/rest/DemoController.java
@@ -11,11 +11,9 @@ public class DemoController {
 
     private Coach myCoach;
 
-    // @Qualifier annotation has a higher priority than @Primary annotation
-    // so if a class is annotated with @Primary, and we specify a @Qualifier
-    // the bean specified with @Qualifier will be used
     @Autowired
-    public DemoController(Coach coach) {
+    public DemoController(@Qualifier("trackCoach") Coach coach) {
+        System.out.println("In constructor: " + this.getClass().getSimpleName());
         myCoach = coach;
     }
 

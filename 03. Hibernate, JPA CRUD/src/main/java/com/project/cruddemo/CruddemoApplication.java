d38493602555cebd8290d797f1c347132eb85638
solunska <a_solunska@outlook.com>
2024-11-25 17:24:58 +0100

    saving a java object

--- 03. Hibernate, JPA CRUD/src/main/java/com/project/cruddemo/CruddemoApplication.java
+++ 03. Hibernate, JPA CRUD/src/main/java/com/project/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.project.cruddemo;
 
+import com.project.cruddemo.dao.StudentDAO;
+import com.project.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,15 @@ public class CruddemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args){
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("Hello World");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        Student student = new Student("Aneta", "Solunska", "aneta@gmail.com");
+        studentDAO.save(student);
+        System.out.println("Saved student with id: " + student.getId());
     }
 }

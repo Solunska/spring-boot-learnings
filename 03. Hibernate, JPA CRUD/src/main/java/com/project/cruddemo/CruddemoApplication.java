5c671675666b5398356fa4110adab95f3511974d
solunska <a_solunska@outlook.com>
2024-11-25 17:28:58 +0100

    testing auto increment

--- 03. Hibernate, JPA CRUD/src/main/java/com/project/cruddemo/CruddemoApplication.java
+++ 03. Hibernate, JPA CRUD/src/main/java/com/project/cruddemo/CruddemoApplication.java
@@ -17,7 +17,9 @@ public class CruddemoApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            createStudent(studentDAO);
+            // createStudent(studentDAO);
+
+            createMultipleStudents(studentDAO);
         };
     }
 
@@ -25,5 +27,15 @@ public class CruddemoApplication {
         Student student = new Student("Aneta", "Solunska", "aneta@gmail.com");
         studentDAO.save(student);
         System.out.println("Saved student with id: " + student.getId());
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+        Student student1 = new Student("John", "Doe", "john@gmail.com");
+        Student student2 = new Student("Mary", "Public", "mary@gmail.com");
+        Student student3 = new Student("Bonita", "Applebum", "bonita@gmail.com");
+
+        studentDAO.save(student1);
+        studentDAO.save(student2);
+        studentDAO.save(student3);
     }
 }

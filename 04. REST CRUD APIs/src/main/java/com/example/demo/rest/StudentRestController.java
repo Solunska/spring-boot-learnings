140334649f6da554232cbefd1307dd28e0f111f4
solunska <a_solunska@outlook.com>
2024-11-26 16:21:15 +0100

    path variables

--- 04. REST CRUD APIs/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 04. REST CRUD APIs/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.example.demo.rest;
 
 import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,24 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void init() {
+        students = new ArrayList<>();
 
         students.add(new Student("Aneta", "Solunska"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id) {
+        return students.get(id);
     }
 }

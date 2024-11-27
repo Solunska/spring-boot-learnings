fa9b116c02314ef7a122db6828f0aa4b4b1a88ba
solunska <a_solunska@outlook.com>
2024-11-27 15:50:28 +0100

    spring data REST configs and sorting

--- 05. REST CRUD Employees/src/main/java/com/example/cruddemo/dao/EmployeeRepository.java
+++ 05. REST CRUD Employees/src/main/java/com/example/cruddemo/dao/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.example.cruddemo.dao;
 import com.example.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }

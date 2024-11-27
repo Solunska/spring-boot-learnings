1b727d8462d6cb9987e2f0c65513e8dcaf84a888
solunska <a_solunska@outlook.com>
2024-11-27 13:04:26 +0100

    defining service layer

--- 05. REST CRUD Employees/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
+++ 05. REST CRUD Employees/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,7 @@ package com.example.cruddemo.rest;
 
 import com.example.cruddemo.dao.EmployeeDAO;
 import com.example.cruddemo.entity.Employee;
+import com.example.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +13,14 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
-    public EmployeeRestController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.getAll();
+        return employeeService.getAll();
     }
 }

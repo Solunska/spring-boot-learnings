f6a31ef1b65272de80321a528176350c94485e29
solunska <a_solunska@outlook.com>
2024-11-27 12:02:10 +0100

    exception handling

--- 04. REST CRUD APIs/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 04. REST CRUD APIs/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.example.demo.rest;
 
 import com.example.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +31,31 @@ public class StudentRestController {
 
     @GetMapping("/students/{id}")
     public Student getStudent(@PathVariable int id) {
+        if ((id >= students.size()) || (id < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + id);
+        }
+
         return students.get(id);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(e.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    // generic exception
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(e.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
